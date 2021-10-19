2a5c3f88d6bd86e1cae7886bc1af2cffdbccb0c6
Rayhan Sheikh <rsheikh201031@mscse.uiu.ac.bd>
2021-10-19 23:55:50 +0600

    comment and responsive

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -12,12 +12,12 @@ const About = () => {
                 at Our Dental Office</h2>
             </div>
             
-            <div className='about-aim body-size'>
+            {/* <div className='about-aim body-size'>
                 <p>
                 Do you want a brighter, healthier smile? Our highly trained dentists offer general, cosmetic, and restorative dentistry treatments at Dental Square to help you achieve the healthiest and most beautiful smile possible. As a patient here, you can rest assured that the entire staff is committed to providing the highest quality care and affordable treatments.
                 </p>
-            </div>
-            <div className='about-doctor body-size'>
+            </div> */}
+            <div className='body-size'>
                 <h2>About Our Dentist</h2>
                 <h3>Dr. Samir Banik</h3>
                 <h5>Assistant Professor<br/>
@@ -25,17 +25,8 @@ const About = () => {
                     Mirpur-14, Dhaka.<br/>
                     Ph.D from Hiroshima University, Japan
                 </h5><br/>
-                <img className='dr-img' src={img} alt='' />
-                <p>Dr. Samir Banik completed his Bachelor of Dental Surgery from the University of Dhaka, Bangladesh in the year 2001. After that, he joined Bangabandhu Sheikh Mujib Medical University for his post graduation training in the dept. of Prosthodontics. Then he joined Bangladesh Civil Service (BCS) as a dental surgeon. At the same time, he was blessed by a prestigious scholarship from Govt. of Japan (Monubukagakasu). Then he joined Hiroshima University, Japan in the dept of Oral & Maxillofacial Radiology as a Ph.d fellow. He finished Doctor of Philosophy in dental science in the year of 2010. Returning from Japan, he joined Dhaka Dental College and Hospital in the dept of Oral and Maxillofacial Radiology as Assistant Professor. He is a specialist in Maxillofacial Radiology. He has a vast working experience and expertise in the below arenas:
-
-                    1. General Dentistry
-                    2. Implant Surgery
-                    3. Prosthodontics
-
-                    Dr. Samir attended at many international congresses around the world. He visited China, Japan, Thailand and Netherland for different issues with dentistry.
-
-                    He is reachable at the e-mail: sbanik30@yahoo.com and also in the contact number: +8801753057342.
-                </p>
+                <img className='dr-img mb-5' src={img} alt='' />
+                
                
             </div>
 
